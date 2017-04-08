b13253176b336776c5c049e4fe671c47d5f405fd
yanxianghe1992 <yanxianghe1992@qq.com>
2017-04-08 15:32:55 +0800

    Initial commit

---
+++ app/src/main/java/com/yxh/yixiuge/app/MyApplication.java
@@ -0,0 +1,14 @@
+package com.yxh.yixiuge.app;
+
+import android.app.Application;
+
+/**
+ * Created by zykj on 2017/4/8.
+ */
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ app/src/main/java/com/yxh/yixiuge/base/BaseActivity.java
@@ -0,0 +1,10 @@
+package com.yxh.yixiuge.base;
+
+import android.app.Activity;
+
+/**
+ * Created by zykj on 2017/4/8.
+ */
+
+public class BaseActivity extends Activity {
+}
